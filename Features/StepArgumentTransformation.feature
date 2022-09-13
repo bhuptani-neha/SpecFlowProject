7c7e8e32fbe221c0c13b7258d3d8ffb473b4c752
bhuptani-neha <bhuptani.neha.07@gmail.com>
2022-09-13 20:40:23 +0530

    BDD Specflow Project update for step argument conversion

---
+++ Features/StepArgumentTransformation.feature
@@ -0,0 +1,9 @@
+﻿Feature: StepArgumentTransformation
+	Simple calculator for adding two numbers
+
+@mytag
+Scenario: Calculate total days between two monthsby converting month to date of current year
+	Given the first input is given as June month
+	And the second input is given as July month
+	Then the total days should be given  61 days
+ 
---
+++ Steps/StepArgumentTransformationStepsDefinitions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace SpecFlowProject1.Steps
+{
+    [Binding]
+    public sealed class StepArgumentTransformationStepsDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public StepArgumentTransformationStepsDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Pending();
+        }
+
+        [StepArgumentTransformation(@"(?:([a-zA-Z]+) month)")]
+        public TimeSpan convertMonthToDays(string month)
+        {
+            
+            Console.WriteLine(month);
+            int days = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.ParseExact(month, "MMMM", new CultureInfo("en-US")).Month);
+            return new TimeSpan(days,0,0,0);
+        }
+
+
+        [Given(@"the first input is given as (.*)")]
+        public void GivenTheFirstInputIsGivenAsJuneMonth(TimeSpan days)
+        {
+            Console.WriteLine(days);
+            _scenarioContext.Add("first", days);
+        }
+
+        [Given(@"the second input is given as (.*)")]
+        public void GivenTheSecondInputIsGivenAsJulyMonth(TimeSpan days)
+        {
+            Console.WriteLine(days);
+            _scenarioContext.Add("second", days);
+        }
+
+        [Then(@"the total days should be given  (.*) days")]
+        public void ThenTheTotalDaysShouldBeGivenDays(int p0)
+        {
+            Console.WriteLine(p0);
+            int days = _scenarioContext.Get<TimeSpan>("first").Days + _scenarioContext.Get<TimeSpan>("second").Days;
+            Console.WriteLine(days);
+            p0.Should().Be(days);
+        }
+
+    }
+}
